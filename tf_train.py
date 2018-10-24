47546899d9a098631bc014649c53bc5db2d62546
xuyuewei <xyw930511@live.com>
2018-10-24 16:25:30 +0800

    Just created the model

---
+++ tf_train.py
@@ -0,0 +1,49 @@
+import argparse
+import tf_carvana_unet_model,load_image_data
+import bce_dice_loss
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--save_weights_path", type = str  )
+parser.add_argument("--train_images", type = str  )
+parser.add_argument("--train_annotations", type = str  )
+parser.add_argument("--n_classes", type=int )
+parser.add_argument("--input_height", type=int , default = 224  )
+parser.add_argument("--input_width", type=int , default = 224 )
+
+parser.add_argument('--validate',action='store_false')
+parser.add_argument("--val_images", type = str , default = "")
+parser.add_argument("--val_annotations", type = str , default = "")
+
+parser.add_argument("--epochs", type = int, default = 5 )
+parser.add_argument("--batch_size", type = int, default = 2 )
+parser.add_argument("--val_batch_size", type = int, default = 2 )
+parser.add_argument("--load_weights", type = str , default = "")
+
+parser.add_argument("--model_name", type = str , default = "")
+parser.add_argument("--optimizer_name", type = str , default = "adadelta")
+
+
+args = parser.parse_args()
+
+train_images_path = args.train_images
+train_segs_path = args.train_annotations
+train_batch_size = args.batch_size
+n_classes = args.n_classes
+input_height = args.input_height
+input_width = args.input_width
+validate = args.validate
+save_weights_path = args.save_weights_path
+epochs = args.epochs
+load_weights = args.load_weights
+
+optimizer_name = args.optimizer_name
+model_name = args.model_name
+
+if validate:
+	val_images_path = args.val_images
+	val_segs_path = args.val_annotations
+	val_batch_size = args.val_batch_size
+  
+model = tf_carvana_unet_model()
