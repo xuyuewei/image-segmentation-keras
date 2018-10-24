4bb013b5107cc814082453caa94672e7a98bd731
xuyuewei <xyw930511@live.com>
2018-10-24 11:48:06 +0800

    add bce_dice_loss

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import argparse
 import Models , LoadBatches
+import bce_dice_loss
 
 
 
@@ -52,7 +53,9 @@ m = modelFN( n_classes , input_height=input_height, input_width=input_width   )
 m.compile(loss='categorical_crossentropy',
       optimizer= optimizer_name ,
       metrics=['accuracy'])
-
+#m.compile(loss='categorical_crossentropy',
+      optimizer= optimizer_name ,
+      metrics=['accuracy'])
 
 if len( load_weights ) > 0:
 	m.load_weights(load_weights)
