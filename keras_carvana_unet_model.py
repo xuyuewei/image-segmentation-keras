758ce0fd2bd461e21e0659358611d893d844aeb1
xuyuewei <xyw930511@live.com>
2018-10-25 00:37:55 +0800

    Update and rename tf_carvana_unet_model.py to keras_carvana_unet_model.py

--- keras_carvana_unet_model.py
+++ keras_carvana_unet_model.py
@@ -1,29 +1,48 @@
 import bce_dice_loss
 
+from tensorflow.keras import layers as kl
+from tensorflow.keras.models import Model
+
 import numpy as np
 import tensorflow as tf
-import tensorflow.contrib as tfcontrib
-from tensorflow.python.keras import layers
-from tensorflow.python.keras import losses
-from tensorflow.python.keras import models
-from tensorflow.python.keras import backend as K
 
 def conv_block(input_tensor, num_filters):
+    encoder = kl.Conv2D(num_filters, (3, 3), strides=(1, 1), padding='same')(input_tensor)
+    encoder = kl.BatchNormalization()(encoder)
+    encoder = kl.ReLU()(encoder)
+    encoder = kl.Conv2D(num_filters, (3, 3), strides=(1, 1), padding='same')(encoder)
+    encoder = kl.BatchNormalization()(encoder)
+    encoder = kl.ReLU()(encoder)
+    '''
+    #tf implemention
+    encoder = layers.Activation('relu')(encoder)
     encoder = layers.Conv2D(num_filters, (3, 3), padding='same')(input_tensor)
     encoder = layers.BatchNormalization()(encoder)
     encoder = layers.Activation('relu')(encoder)
     encoder = layers.Conv2D(num_filters, (3, 3), padding='same')(encoder)
     encoder = layers.BatchNormalization()(encoder)
-    encoder = layers.Activation('relu')(encoder)
+    '''
     return encoder
 
 def encoder_block(input_tensor, num_filters):
     encoder = conv_block(input_tensor, num_filters)
-    encoder_pool = layers.MaxPooling2D((2, 2), strides=(2, 2))(encoder)
+    encoder_pool = kl.MaxPooling2D(pool_size=(2,2), strides=(2, 2))(encoder)
+    #encoder_pool = layers.MaxPooling2D((2, 2), strides=(2, 2))(encoder)
     
     return encoder_pool, encoder
     
 def decoder_block(input_tensor, concat_tensor, num_filters):
+    decoder = kl.Conv2DTranspose(num_filters, (2, 2), strides=(2, 2), padding='same')(input_tensor)
+    decoder = kl.Concatenate(axis=-1)([concat_tensor, decoder])
+    decoder = kl.BatchNormalization()(decoder)
+    decoder = kl.ReLU()(decoder)
+    decoder = kl.Conv2D(num_filters, (3, 3), strides=(1, 1), padding='same')(decoder)
+    decoder = kl.BatchNormalization()(decoder)
+    decoder = kl.ReLU()(decoder)
+    decoder = kl.Conv2D(num_filters, (3, 3), strides=(1, 1), padding='same')(decoder)
+    decoder = kl.BatchNormalization()(decoder)
+    decoder = kl.ReLU()(decoder)
+    '''
     decoder = layers.Conv2DTranspose(num_filters, (2, 2), strides=(2, 2), padding='same')(input_tensor)
     decoder = layers.concatenate([concat_tensor, decoder], axis=-1)
     decoder = layers.BatchNormalization()(decoder)
@@ -34,10 +53,11 @@ def decoder_block(input_tensor, concat_tensor, num_filters):
     decoder = layers.Conv2D(num_filters, (3, 3), padding='same')(decoder)
     decoder = layers.BatchNormalization()(decoder)
     decoder = layers.Activation('relu')(decoder)
+    '''
     return decoder
 
-def unet(img_shape=(360,480),loss = bce_dice_loss,optimizer='adam',metrics=[dice_loss]):
-    inputs = layers.Input(shape=img_shape)
+def unet(img_shape,loss = bce_dice_loss,optimizer='adam',metrics=[dice_loss]):
+    inputs = kl.Input(shape=img_shape)
     # 256
     encoder0_pool, encoder0 = encoder_block(inputs, 32)
     # 128
@@ -63,7 +83,7 @@ def unet(img_shape=(360,480),loss = bce_dice_loss,optimizer='adam',metrics=[dice
     # 256 
     outputs = layers.Conv2D(1, (1, 1), activation='sigmoid')(decoder0)
     
-    model = models.Model(inputs=[inputs], outputs=[outputs])
+    model = Model(inputs=[inputs], outputs=[outputs])
     
     model.summary()
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
