5fa7147bc48424f0058c098f5ea117c5bbe25374
xuyuewei <xyw930511@live.com>
2018-10-26 00:01:21 +0800

    Update tf_train_unet.py

--- tf_train_unet.py
+++ tf_train_unet.py
@@ -6,14 +6,10 @@ import tf_img_prepro_aug
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--save_weights_path", type = str  )
-parser.add_argument("--train_images_path", type = str  )
-parser.add_argument("--train_segs_path", type = str  )
+parser.add_argument("--images_path", type = str  )
+parser.add_argument("--labels_path", type = str  )
 parser.add_argument("--input_height", type=int , default = 256  )
 parser.add_argument("--input_width", type=int , default = 256 )
-
-parser.add_argument('--validate',action='store_false')
-parser.add_argument("--val_images_path", type = str , default = "")
-parser.add_argument("--val_segs_path", type = str , default = "")
 
 parser.add_argument("--epochs", type = int, default = 5 )
 parser.add_argument("--batch_size", type = int, default = 2 )
@@ -22,19 +18,25 @@ parser.add_argument("--load_weights_path", type = str , default = "")
 
 args = parser.parse_args()
 
-train_images_path = args.train_images
-train_segs_path = args.train_segs_path
-train_batch_size = args.batch_size
+images_path = args.images_path
+labels_path = args.labels_path
+batch_size = args.batch_size
 input_height = args.input_height
 input_width = args.input_width
 validate = args.validate
 save_weights_path = args.save_weights_path
 epochs = args.epochs
-load_weights = args.load_weights_path
+load_weights_path = args.load_weights_path
 
 if validate:
-	val_images_path = args.val_images_path
-	val_segs_path = args.val_segs_path
-	val_batch_size = args.val_batch_size
-  
+    
+ 
+img_data = tf.data.Dataset.list_files(images_path)
+img_data = img_data.shuffle(500)
+img_data = img_data.map(lambda x: load_jpeg(x))
+img_data = img_data.batch(1)
+labels_data = tf.data.Dataset.list_files(labels_path)
+labels_data = labels_data.shuffle(500)
+labels_data = labels_data.map(lambda x: load_jpeg(x))
+labels_data = labels_data.batch(1)
 model = tf_unet_model()
