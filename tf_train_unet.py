5c888e886c786bf64f77aae3e7f845fddda4ea9c
xuyuewei <xyw930511@live.com>
2018-10-26 16:46:27 +0800

    Update tf_train_unet.py

--- tf_train_unet.py
+++ tf_train_unet.py
@@ -28,15 +28,30 @@ save_weights_path = args.save_weights_path
 epochs = args.epochs
 load_weights_path = args.load_weights_path
 
+
+
+num_of_samples = len(img_array)
+img_array = tf.data.Dataset.list_files(images_path,shuffle=False)
+labels_array = tf.data.Dataset.list_files(labels_path,shuffle=False)
+img_labels = tf.data.Dataset.from_tensor_slices((img_array,labels_array))
+img_labels = img_labels.shuffle(num_of_samples)
+
+#train_val_split
+val_labels_data = None
 if validate:
+    val_array = img_labels[:num_of_samples//10]
+    img_labels = img_labels[num_of_samples//10:]
+    val_data = val_array.map(lambda x: (load_jpeg(x[0])[:,:input_width//2],load_jpeg(x[0])[:,input_width//2:]))
+    val_data = val_data.batch(1)
     
- 
-img_data = tf.data.Dataset.list_files(images_path)
-img_data = img_data.shuffle(500)
-img_data = img_data.map(lambda x: load_jpeg(x))
-img_data = img_data.batch(1)
-labels_data = tf.data.Dataset.list_files(labels_path)
-labels_data = labels_data.shuffle(500)
-labels_data = labels_data.map(lambda x: load_jpeg(x))
-labels_data = labels_data.batch(1)
+img_labels_data = img_labels.map(lambda x: (load_jpeg(x[0])[:,:input_width//2],load_jpeg(x[0])[:,input_width//2:]))    
+img_labels_data = img_labels_data.batch(1)
+
+#create unet model
 model = tf_unet_model()
+                                         
+
+
+
+
+
